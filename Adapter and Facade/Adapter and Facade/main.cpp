984ae126c70ee61ffc9f04950c74d924216fa889
Brian Arnold <mrlegowatch@mac.com>
2018-02-04 08:26:50 -0500

    Add projects for chapters 1 thru 7.

---
+++ Adapter and Facade/Adapter and Facade/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Adapter and Facade
+//
+//  Created by Brian Arnold on 2/4/18.
+//  Copyright © 2018 Brian Arnold. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
